403c8aa0779301b4b13e8bbbf57b1fbb276921a4
Kazungu1 <kazunguderriq@gmail.com>
2023-03-03 10:46:00 +0300

    override default table names

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -11,5 +11,13 @@ namespace ContosoUniversity.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Stuents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
